e0915b33c3fe70374c2a888e3bf8bf261d0d9f99
Wakeel Kasali <kasaliwakeeladekunle@gmail.com>
2025-07-07 19:34:39 -0700

    add sql data load script

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,7 +1,7 @@
 ALTER TABLE drivebcevents DROP CONSTRAINT IF EXISTS drivebcevents_pkey;
 
 -- Load the data from CSV file
--- Adjust the path as necessary
+-- Modify the path as necessary
 \COPY drivebcevents FROM '/absolute/path/to/drivebc_2020_2024_cleaned.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',');
 
 -- Verify number of rows inserted
